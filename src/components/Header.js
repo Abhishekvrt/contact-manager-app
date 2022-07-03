65a1076c1ff068abd3ec3e81062f61c6e56d5480
Abhishek-Kumar <abhishekphd14@gmail.com>
2022-07-03 15:28:16 +0530

    main

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function 
+() {
+  return (
+    <div className="ui main menu">
+      <div className="ui container">
+         <h2>Contact Manager App</h2>
+      </div>
+      
+    </div>
+  )
+}
